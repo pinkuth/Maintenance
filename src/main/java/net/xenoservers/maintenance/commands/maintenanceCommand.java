a9573878de50f3a47bee951a396802ffb0635825
ShmexyPink <shmexypink@gmail.com>
2021-09-08 22:05:57 +0100

    Fixed most bugs but data doesnt get saved in config

--- src/main/java/net/xenoservers/maintenance/commands/maintenanceCommand.java
+++ src/main/java/net/xenoservers/maintenance/commands/maintenanceCommand.java
@@ -3,6 +3,7 @@ package net.xenoservers.maintenance.commands;
 import dev.waterdog.waterdogpe.command.Command;
 import dev.waterdog.waterdogpe.command.CommandSender;
 import dev.waterdog.waterdogpe.command.CommandSettings;
+import dev.waterdog.waterdogpe.player.ProxiedPlayer;
 
 import net.xenoservers.maintenance.Maintenance;
 
@@ -20,21 +21,30 @@ public class maintenanceCommand extends Command {
 
     @Override
     public boolean onExecute(CommandSender sender, String alias, String[] args) {
+        if(args.length > 1) {
+            return false;
+        }
         if (args[0].equals("enable") || args[0].equals("true")) {
-            if (this.main.maintenanceStatus == true) {
+            if (this.main.maintenanceStatus) {
                 sender.sendMessage("§eMaintenance mode is already enabled!");
-                return false;
+                return true;
             } else {
                 this.main.getConfig().setBoolean("enabled", true);
+                this.main.saveResource("config.yml");
+                for (ProxiedPlayer proxiedPlayer : this.main.getProxy().getPlayers().values()) {
+                    if(!proxiedPlayer.hasPermission("maintenance.join")) proxiedPlayer.disconnect(this.main.maintenanceMsg);
+                }
+
                 sender.sendMessage("§aMaintenance mode has been enabled!");
                 return true;
             }
         } else if (args[0].equals("disable") || args[0].equals("false")) {
-            if (this.main.maintenanceStatus == false) {
+            if (!this.main.maintenanceStatus) {
                 sender.sendMessage("§eMaintenance mode is already disabled!");
-                return false;
+                return true;
             } else {
                 this.main.getConfig().setBoolean("enabled", false);
+                this.main.saveResource("config.yml");
                 sender.sendMessage("§aMaintenance mode has been disabled!");
                 return true;
             }
